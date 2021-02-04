0995d93d780a2b0f0b986b7e2472d996e8a40193
Chia-Hua Lu <krisswolf.kw@gmail.com>
2021-02-04 15:03:42 -0600

    Add Scroll View to Config Screen

--- src/components/screens/cameraConfiguration/CameraConfigurationScreen.js
+++ src/components/screens/cameraConfiguration/CameraConfigurationScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Image, SafeAreaView } from 'react-native';
+import { View, Image, SafeAreaView, ScrollView } from 'react-native';
 import { Text, Button, Input } from 'react-native-elements';
 import { Actions } from 'react-native-router-flux';
 
@@ -25,53 +25,55 @@ class CameraConfigurationScreen extends Component {
 	render() {
 		return (
 			<SafeAreaView>
-				<Text style={styles.instruction}>{strings.cameraConfigInstruction1}</Text>
-				<Text style={styles.instruction}>{strings.cameraConfigInstruction2}</Text>
-				<View style={styles.contentContainer}>
-					<View style={styles.cameraAngleConfigContainer}>
-						<Text h3 style={styles.angleName}>{strings.cameraAngleHorizontalSectionLabel}</Text>
-						<Image style={styles.angleDiagram} source={HorizontalAngleOfViewDiagram} />
-						<Input
-							label={strings.cameraHorizontalAngleInputLabel}
-							placeholder={"40"}
-							value={`${this.state.horizontalAngleOfView}`}
-							keyboardType="decimal-pad"
-							onChangeText={(newValue) => {
-								this.setState({...this.state, horizontalAngleOfView: newValue});
-							}}/>
+				<ScrollView>
+					<Text style={styles.instruction}>{strings.cameraConfigInstruction1}</Text>
+					<Text style={styles.instruction}>{strings.cameraConfigInstruction2}</Text>
+					<View style={styles.contentContainer}>
+						<View style={styles.cameraAngleConfigContainer}>
+							<Text h3 style={styles.angleName}>{strings.cameraAngleHorizontalSectionLabel}</Text>
+							<Image style={styles.angleDiagram} source={HorizontalAngleOfViewDiagram} />
+							<Input
+								label={strings.cameraHorizontalAngleInputLabel}
+								placeholder={"40"}
+								value={`${this.state.horizontalAngleOfView}`}
+								keyboardType="decimal-pad"
+								onChangeText={(newValue) => {
+									this.setState({...this.state, horizontalAngleOfView: newValue});
+								}}/>
+						</View>
+						<View style={styles.cameraAngleConfigContainer}>
+							<Text h3 style={styles.angleName}>{strings.cameraAngleVerticalSectionLabel}</Text>
+							<Image style={styles.angleDiagram} source={VerticalAngleOfViewDiagram} />
+							<Input
+								label={strings.cameraVerticalAngleInputLabel}
+								placeholder={"55"}
+								value={`${this.state.verticalAngleOfView}`}
+								keyboardType="decimal-pad"
+								onChangeText={(newValue) => {
+									this.setState({...this.state, verticalAngleOfView: newValue});
+								}}/>
+						</View>
 					</View>
-					<View style={styles.cameraAngleConfigContainer}>
-						<Text h3 style={styles.angleName}>{strings.cameraAngleVerticalSectionLabel}</Text>
-						<Image style={styles.angleDiagram} source={VerticalAngleOfViewDiagram} />
-						<Input
-							label={strings.cameraVerticalAngleInputLabel}
-							placeholder={"55"}
-							value={`${this.state.verticalAngleOfView}`}
-							keyboardType="decimal-pad"
-							onChangeText={(newValue) => {
-								this.setState({...this.state, verticalAngleOfView: newValue});
-							}}/>
+					<View style={styles.buttonsContainer}>
+						<Button
+							buttonStyle={styles.buttonStyle}
+							onPress={() => {
+								this.props.resetCameraAngleOfView();
+								this.setState({...this.props.configuration.camera})
+							}}
+						 	title={strings.resetButtonText}/>
+						<Button
+							buttonStyle={styles.buttonStyle}
+							onPress={() => {
+								this.props.setCameraAngleOfView({
+									horizontalAngleOfView: this.state.horizontalAngleOfView,
+									verticalAngleOfView: this.state.verticalAngleOfView,
+								});
+								Actions.pop();
+							}}
+						 	title={strings.saveButtonText}/>
 					</View>
-				</View>
-				<View style={styles.buttonsContainer}>
-					<Button
-						buttonStyle={styles.buttonStyle}
-						onPress={() => {
-							this.props.resetCameraAngleOfView();
-							this.setState({...this.props.configuration.camera})
-						}}
-					 	title={strings.resetButtonText}/>
-					<Button
-						buttonStyle={styles.buttonStyle}
-						onPress={() => {
-							this.props.setCameraAngleOfView({
-								horizontalAngleOfView: this.state.horizontalAngleOfView,
-								verticalAngleOfView: this.state.verticalAngleOfView,
-							});
-							Actions.pop();
-						}}
-					 	title={strings.saveButtonText}/>
-				</View>
+				</ScrollView>
 			</SafeAreaView>
 		);
 	}
